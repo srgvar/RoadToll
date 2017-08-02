9873c8da0c629fd4dd917bd1bc79fa73996028b6
Сергей Варакин <srgvar@narod.ru>
2017-08-02 22:02:30 +0700

    Начало server-core

--- server-core/src/main/java/jdev/server/main/ServerCore.java
+++ server-core/src/main/java/jdev/server/main/ServerCore.java
@@ -1,21 +1,20 @@
-package jdev.server.app;
+package jdev.server.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by srgva on 18.07.2017.
  */
+// @Configuration
 
-public class Main {
-
+@SpringBootApplication
+@ComponentScan({"jdev.server.main",
+                "jdev.server.controllers",
+                "jdev.server.test"})
+public class ServerCore {
     public static void main(String[] args) {
-
-        System.out.println("Server core");;
+        SpringApplication.run(ServerCore.class, args);
     }
-
 }
